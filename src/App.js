96e8fae275b68b53d866cada86531b9c6b205d8d
gonzalo-lemoine <glemoine@udesa.edu.ar>
2023-08-28 17:26:56 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 
 function App() {
+
   return (
+    <React.Fragment>
     <Header/>
+    
+    <Footer/>
+    </React.Fragment>
+
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Footer.css';
+
+function Footer(props) {
+    return(
+
+        <footer>
+        <article className="logo"><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="Logoapi " className="logodelye"/>
+    
+        <div className="footnombre">
+        <p>Sitio web diseñado y programado por: Bautista Kamijo, Ariel Azambuja y Gonzalo Lemoine</p></div> 
+    </article>
+        
+    
+    </footer>
+    
+
+    )};
+
+    export default Footer;
