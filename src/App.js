28321aa477d4832087e34ae885d85acbffa1970a
ArielAzambuja <aazambuja@udesa.edu.ar>
2023-09-04 15:50:56 -0300

    ROUTEO

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Home from './screens/Home/Home';
+import Favorites from './screens/Favorites/Favorites';
+import Generos from './screens/Generos/Generos';
+import Detalle from './screens/Detalle/Detalle';
+import NotFound from './screens/NotFound/NotFound';
+
 
 
 function App() {
@@ -8,6 +15,13 @@ function App() {
   return (
     <React.Fragment>
     <Header/>
+    <Switch>
+      <Route path='/' exact={true} component={Home} />
+      <Route path='/detalles/:id' component={Detalle} />
+      <Route path='/favorites' component={Favorites} />
+      <Route path='/generos' exact={true} component={Generos} />
+      <Route path='' component={NotFound} />
+    </Switch>
     
     <Footer/>
     </React.Fragment>
