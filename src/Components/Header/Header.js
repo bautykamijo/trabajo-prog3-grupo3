cd4e04982d4f1a6e9cb05838d4f29092ae36864e
bautykamijo <bkamijo@udesa.edu.ar>
2023-09-04 14:47:08 -0300

    listrorti

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,22 +5,21 @@ function Header(props) {
     return(
 
         <header>
-    <article class="logo"><img src="./img/logoDelyeMovies.png" alt="Logo DelyeMovies" class="logodelye"/></article>
-
+    <article className="logo"><img src="./img/logoDelyeMovies.png" alt="Logo DelyeMovies" className="logodelye"/></article>
 
     <nav>
-    <ul class="barranavheader">
-            <li class="itemshome"><a href="./index.html" class="ahrefheader">HOME</a></li>
-            <li class="itemshome"><a href="./favorite.html" class="ahrefheader">FAVORITOS</a></li>
-            <li class="itemshome"><a href="./genres.html" class="ahrefheader">GENEROS</a></li>
+    <ul className="barranavheader">
+            <li className="itemshome"><a href="./index.html" className="ahrefheader">HOME</a></li>
+            <li className="itemshome"><a href="./favorite.html" className="ahrefheader">FAVORITOS</a></li>
+            <li className="itemshome"><a href="./genres.html" className="ahrefheader">GENEROS</a></li>
          </ul>
     </nav>
 
-   <div class="divheader">
+    <div className="divheader">
     <form action="./search-results.html" method="get">
-        <input class="buscadoryboton" type="search" name="buscador" value="" placeholder="Peliculas, series..."/>
+        <input className="buscadoryboton" type="search" name="buscador" value="" placeholder="Peliculas, series..."/>
     </form>
-</div>
+    </div>
     </header>
 
     )};
