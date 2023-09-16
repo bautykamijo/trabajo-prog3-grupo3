72d452df099f2b1c68ef736d7426d6acf3e08c74
gonzalo-lemoine <glemoine@udesa.edu.ar>
2023-09-15 22:17:29 -0300

    ultimos

--- src/screens/VerTodas/VerUpcoming.js
+++ src/screens/VerTodas/VerUpcoming.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import ContainerTodas from "../../Components/Body/ContainerTodas/ContainerTodas";
+import Filtrador from "../../Components/Body/Filtrador/Filtrador";
 import "./VerTodas.css"
 
 class VerUpcoming extends Component {
@@ -23,13 +24,33 @@ class VerUpcoming extends Component {
         .catch( error => console.log('El error fue: ' + error))
                                                                 }
 
+         filtradorPeliculas = (texto) => {
+        let filtradas = this.state.popular.filter((movie) =>
+        movie.title.toUpperCase().includes(texto.toUpperCase()))
+
+        this.setState({
+            popular: filtradas,
+        })
+    }
+
+    traerMas(){
+        let numero = this.state.pagina;
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=20ad67ce31acb5c646fe21c26a0d44f1&language=en-US&page=${numero}`)
+            .then((response) => response.json())
+            .then((data) => this.setState(
+            { popular: data.results.concat(this.state.popular),
+                page: numero + 1})
+            )
+            .catch((error) => console.log(error));
+    }
 
 
     render(){
         
         return(
             <main>
-                <h2 className="espaciadoTituloUno">Peliculas en Cartelera</h2> 
+                 <Filtrador filtrador={(texto)=> this.filtradorPeliculas(texto)}/>
+                <h2 className="espaciadoTituloUno">Peliculas en Cartelera <button className="favorites favoritismo" onClick={() => this.traerMas()}> Traer mas </button></h2> 
                 <ContainerTodas movies={this.state.upcoming}/>
                 <br></br><br></br>
             </main>
